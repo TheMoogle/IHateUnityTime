9f8895e5fc626a902e5b24caf1eae64442fe6eda
TheMoogle <that1moogle@gmail.com>
2024-10-22 16:45:33 -0500

    Port To Pure C
    Rewrite library to not rely on c++ only on libc

--- library.c
+++ library.c
@@ -2,15 +2,14 @@
 
 #if defined(_WIN32) || defined(_WIN64)
     // Windows specific includes
+    #define WIN32_LEAN_AND_MEAN
     #include <windows.h>
 #else
     // Linux specific includes
     #include <locale.h>
     #include <langinfo.h>
-    #include <string>
+    #include <string.h>
 #endif
-
-#include <iostream>
 
 bool Is24HourClock() {
 #if defined(_WIN32) || defined(_WIN64)
@@ -19,25 +18,22 @@ bool Is24HourClock() {
 
     // Retrieve the time format from the user's locale settings
     if (GetLocaleInfoEx(LOCALE_NAME_USER_DEFAULT, LOCALE_STIMEFORMAT, timeFormat, sizeof(timeFormat) / sizeof(wchar_t)) > 0) {
-        std::wstring format(timeFormat);
-
         // Check for the presence of 'H' (24-hour) or 'h' (12-hour)
-        if (format.find(L"H") != std::wstring::npos) {
+        if (wcsstr(timeFormat, L"H") != NULL) {
             return true;  // 24-hour format
         }
     }
     return false;  // 12-hour format
-
 #else
     // Linux code
     // Set locale based on the environment
     setlocale(LC_TIME, "");
 
     // Retrieve the time format string
-    std::string timeFormat = nl_langinfo(T_FMT);
+    const char *timeFormat = nl_langinfo(T_FMT);
 
     // Look for 'H' in the time format string (24-hour format)
-    if (timeFormat.find("%H") != std::string::npos) {
+    if (strstr(timeFormat, "%H") != NULL) {
         return true;  // 24-hour format
     }
     return false;  // 12-hour format
